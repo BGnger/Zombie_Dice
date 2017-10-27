530a9dc7ac38dc211d59f1caa6e28f98ea06fd66
Fermin <ferminsandoval950@hotmail.com>
2017-10-27 17:18:38 -0600

    This is fermin's push

--- ZombieDice/src/PlayGame.java
+++ ZombieDice/src/PlayGame.java
@@ -2,5 +2,6 @@ public class PlayGame {
     public static void main(String[] args){
         System.out.println("Testing, testing, 1, 2, 3.");
         System.out.println("Testing");
+        System.out.println("This is fermin's push");
     }
 }
