9dfdcba998c1d1f3c09b546ddcca1e3b298a281a
Brevlin Thompson <BThompson@student.neumont.edu>
2017-10-30 15:13:27 -0600

    Updated Cup class

--- ZombieDice/src/Cup.java
+++ ZombieDice/src/Cup.java
@@ -1,40 +1,22 @@
 import java.util.Random;
 
 public class Cup {
-    public static void cup (){
-        int Probability = 3;
-        int[] ColorProbability = new int[Probability];
-        Die Dice1 = new Die();
-        Die Dice2 = new Die();
-        Die Dice3 = new Die();
-        for (int i = 0; i < 3; i++) {
+    public static Die[] cup(int numDice){
+        int[] ColorProbability = new int[numDice];
+        Die[] dice = new Die[numDice];
+        for (int i = 0; i < numDice; i++) {
             Random color = new Random();
             int Color = color.nextInt(100) + 1;
             ColorProbability[i] = Color;
+            if (ColorProbability[i] <= 46) {
+                dice[i].setColor("Green");
+            } else if (ColorProbability[i] <= 77) {
+                dice[i].setColor("Yellow");
+            } else if (ColorProbability[i] <= 100) {
+                dice[i].setColor("Red");
+            }
+
         }
-            if (ColorProbability[0] <= 46) {
-                Dice1.setColor("Green");
-            } else if (ColorProbability[0] <= 77) {
-                Dice1.setColor("Yellow");
-            } else if (ColorProbability[0] <= 100) {
-                Dice1.setColor("Red");
-            }
-            if (ColorProbability[1] <= 46) {
-                Dice2.setColor("Green");
-            } else if (ColorProbability[1] <= 77) {
-                Dice2.setColor("Yellow");
-            } else if (ColorProbability[1] <= 100) {
-                Dice2.setColor("Red");
-            }
-            if (ColorProbability[2] <= 46) {
-                Dice3.setColor("Green");
-            } else if (ColorProbability[2] <= 77) {
-                Dice3.setColor("Yellow");
-            } else if (ColorProbability[2] <= 100) {
-                Dice3.setColor("Red");
-            }
-        System.out.println("Your first dice color is " + Dice1.getColor());
-        System.out.println("Your second dice color is " + Dice2.getColor());
-        System.out.println("Your third dice color is " + Dice3.getColor());
+        return dice;
     }
 }
