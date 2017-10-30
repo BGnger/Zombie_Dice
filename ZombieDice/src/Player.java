82b83594bda018eade6691833268cca9f31e8b50
PrimalGod <33164266+PrimalGod@users.noreply.github.com>
2017-10-30 14:56:22 -0600

    Update Player.java

--- ZombieDice/src/Player.java
+++ ZombieDice/src/Player.java
@@ -1,16 +1,6 @@
-import java.util.Scanner;
 public class Player {
 
-    String Player1;
-    String Player2;
-
-
-    Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Player 1, Please enter your Name:");
-
-        System.out.println("Player 2, Please enter your name");
-
-
+    private String Player1;
+    private String Player2;
 }
 
