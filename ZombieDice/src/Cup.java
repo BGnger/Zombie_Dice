9452d47568c29d36fb3860dc0fa3db613abe572a
Lucas <lukeshep42@hotmail.com>
2017-10-31 15:15:55 -0600

    Changed cup to generate 1 die color

--- ZombieDice/src/Cup.java
+++ ZombieDice/src/Cup.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class Cup {
-    public void produceDie(Die die) {
+    public static void produceDie(Die die) {
         Random rand = new Random();
         int colorInt = rand.nextInt(100) + 1;
         if (colorInt<=46) {
