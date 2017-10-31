5afa8f5ce12355c157eae4d0da9ca739a07ee02e
Brevlin Thompson <BThompson@student.neumont.edu>
2017-10-31 15:00:10 -0600

    Update to Die class

--- ZombieDice/src/Die.java
+++ ZombieDice/src/Die.java
@@ -3,22 +3,22 @@
 
      private String[] colorChoice = {"green", "red", "yellow"};
      private String color = "";
+     private String symbol = "";
 
      public Die() {
 
      }
 
-  public void dieRoll(Player player){
+  public void rollColoredDie(Player player){
        int[] green = {3, 5, 6};
        int[] yellow = {2, 4, 6};
        int[] red = {1, 3, 6};
-
        if(this.color.equalsIgnoreCase("green")){
-           rollColoredDie(green, player);
+           dieRoll(green, player);
        }else if(this.color.equalsIgnoreCase("yellow")){
-           rollColoredDie(yellow, player);
+           dieRoll(yellow, player);
        }else if(this.color.equalsIgnoreCase("red")){
-            rollColoredDie(red, player);
+            dieRoll(red, player);
        }
   }
 
@@ -32,16 +32,22 @@
 
   public String getColor(){return color;}
 
-  private void rollColoredDie(int[] stats, Player player){
+  // depending on the int[] called out by the rollColoredDie method, the Color Die will be rolled with different values
+  // based on what the variable color will be.
+  private void dieRoll(int[] stats, Player player){
       Random rand = new Random();
       int randNum = rand.nextInt(6) + 1;
       if(randNum <= stats[0]){
           player.setSurvivors(player.getSurvivors() + 1);
+          this.symbol = "Brain";
       }else if(randNum <= stats[1]){
-
+        this.symbol = "Runner";
       }else if(randNum <= stats[2]){
           player.setShots(player.getShots() + 1);
+          this.symbol = "Shot";
       }
   }
+
+  public String getSymbol(){return symbol;}
  }
 
