db02ef5f2fd4ed853871dace72c7a7937a4d922a
Lucas <lukeshep42@hotmail.com>
2017-11-01 14:29:14 -0600

    Reworked game class

--- ZombieDice/src/Game.java
+++ ZombieDice/src/Game.java
@@ -9,9 +9,13 @@ public class Game {
             Scanner scanner = new Scanner(System.in);
             Player player1 = new Player();
             Player player2 = new Player();
-//        Die die1 = new Die();
-//        Die die2 = new Die();
-//        Die die3 = new Die();
+            Player[] players = new Player[2];
+            for (int i=0;i<players.length;i++) {
+                Player player = new Player();
+                players[i] = player;
+            }
+            Die[] dice = new Die[3];
+
 
             System.out.println("Player 1, please enter your name!");
             player1.setName(scanner.nextLine());
@@ -26,43 +30,41 @@ public class Game {
             }
 
             boolean gameRunning = true;
-            boolean player1Turn = true;
+            boolean turn;
 
-//        int shots = 0;
 
             while (gameRunning) {
-                while (player1Turn) {
-                    System.out.println(player1.getName());
-                    System.out.println(player1.getBrains());
-                    System.out.println(player1.getSurvivors());
-                    System.out.println(player1.getShots());
-                    Die die1 = new Die();
-                    Die die2 = new Die();
-                    Die die3 = new Die();
-                    String continueTurn = scanner.nextLine();
-                    if (continueTurn.equalsIgnoreCase("quit")) {
-                        player1Turn = false;
-                        player1.setBrains(player1.getSurvivors());
-                        player1.setSurvivors(0);
+                for (int x = 0; x < players.length; x++) {
+                    for (int i = 0; i < 3; i++) {
+                        Die die = new Die();
+                        dice[i] = die;
                     }
-                    if (player1.getBrains() >= 13) {
-                        gameRunning = false;
-                        player1Turn = false;
-                    }
-                }
-                while (!player1Turn && gameRunning) {
-                    System.out.println(player2.getName());
-                    System.out.println(player2.getBrains());
-                    System.out.println(player2.getSurvivors());
-                    System.out.println(player2.getShots());
-                    String continueTurn = scanner.nextLine();
-                    if (continueTurn.equalsIgnoreCase("quit")) {
-                        player1Turn = true;
-                        player2.setBrains(player2.getSurvivors());
-                        player2.setSurvivors(0);
-                    }
-                    if (player2.getBrains() >= 13) {
-                        gameRunning = false;
+                    System.out.println("It is "+players[x].getName()+"'s turn!");
+                    turn = true;
+                    while (turn) {
+                        for (int i = 0; i < 3; i++) {
+                            if (!dice[i].getSymbol().equalsIgnoreCase("Runner")) {
+                                Cup.produceDie(dice[i]);
+                                dice[i].rollColoredDie(players[x]);
+                            } else {
+                                dice[i].rollColoredDie(players[x]);
+                            }
+                            System.out.println("Dice number "+(i + 1)+" is a "+dice[i].getColor()+" "+dice[i].getSymbol());
+                        }
+                        System.out.println("So far this turn, you have "+players[x].getSurvivors()+" survivors cornered and "+players[x].getShots()+" shots taken");
+                        System.out.println("Would you like to roll again? [Y/N]");
+                        if (scanner.nextLine().equalsIgnoreCase("N")) {
+                            players[x].setBrains(players[x].getSurvivors());
+                            players[x].setShots(0);
+                            players[x].setSurvivors(0);
+                            System.out.println("You now have a total of "+players[x].getBrains()+" brains");
+                            turn = false;
+                        }
+                        if (players[x].getBrains()>=13) {
+                            System.out.println("Congratulations "+players[x].getName()+"! You win!");
+                            gameRunning = false;
+                            x=players.length;
+                        }
                     }
                 }
             }
